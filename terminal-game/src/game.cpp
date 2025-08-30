ed4c48135faa8c3e392229929f9957ac7f587a3c
Kai-972 <133531757+Kai-972@users.noreply.github.com>
2025-08-30 12:43:13 +0530

    added player

--- terminal-game/src/game.cpp
+++ terminal-game/src/game.cpp
@@ -1,5 +1,6 @@
 #include<string>
 #include<ncurses.h>
+#include<unistd.h>
 
 #include "game.h"
 
@@ -42,9 +43,52 @@ void run() {
 
 	player.disp_char = '0';
 	player.pos = {10, 5};
+
+	int in_char;
+
+	bool exit_requested = false;
+
+	while(1){
+		in_char = wgetch(wnd);
+		mvaddch(player.pos.y, player.pos.x, ' ');
+
+		switch(in_char){
+			case 'q':
+				exit_requested = true;
+				break;
+			case KEY_UP:
+			case 'w':
+				player.pos.y -=1;
+				break;
+			case KEY_DOWN:
+			case 's':
+				player.pos.y +=1;
+				break;
+			case KEY_LEFT:
+			case 'a':
+				player.pos.x -=1;
+				break;
+			case KEY_RIGHT: 
+			case 'd':
+				player.pos.x +=1;
+				break;
+			default:
+				break;
+		}
+
+		mvaddch(player.pos.y, player.pos.x, player.disp_char);
+		refresh();
+
+		if(exit_requested) break;
+
+		usleep(10000);
+
+		refresh();
+	}
 	
-	mvaddch(player.pos.y, player.pos.x, player.disp_char);
-	refresh();
+	// mvaddch(player.pos.y, player.pos.x, player.disp_char);
+	
+	// refresh();
 
 	while(1);
 }
